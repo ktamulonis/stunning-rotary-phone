c53fe44f7b0f8b4a35ee5bcf970a9c54ca4ba19b
ktamulonis <kurttamulonis@gmail.com>
2023-02-11 00:14:25 -0500

    adds ffmpeg

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-import speech_recognition as sr
+import subprocess
+from flask import Flask, request, redirect, url_for, render_template
+from speech_recognition import Recognizer, AudioData
 
 app = Flask(__name__)
 
@@ -7,18 +8,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/transcribe', methods=['POST'])
-def transcribe():
-    audio_file = request.files['audio_file']
-    recognizer = sr.Recognizer()
-    audio = sr.AudioFile(audio_file)
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = file.filename
 
-    with audio as source:
-        audio_data = recognizer.record(source)
+    # Convert the file to PCM WAV format
+    output_filename = filename.split('.')[0] + '.wav'
+    subprocess.call(['ffmpeg', '-i', filename, output_filename])
 
-    text = recognizer.recognize_google(audio_data)
+    # Load the audio file into the SpeechRecognition library
+    recognizer = Recognizer()
+    with open(output_filename, 'rb') as f:
+        audio_data = AudioData(f.read(), recognizer.sample_rate, 2)
 
-    return render_template('transcribe.html', text=text)
+    # Recognize the speech in the audio file
+    transcribed_text = recognizer.recognize_google(audio_data)
+
+    return render_template('result.html', text=transcribed_text)
 
 if __name__ == '__main__':
     app.run()
